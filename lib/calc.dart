1748faf3bfc8dc6be5193563ad56d4dc824403f7
Kishan Kr Sharma <skishan781@gmail.com>
2020-04-20 18:15:10 +0530

    Added AgeCalc,BmiCalc

---
+++ lib/calc.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() => runApp(Calculator());
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "k",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: SimpleCalculator(),
+    );
+  }
+}
+
+class SimpleCalculator extends StatefulWidget {
+  @override
+  _SimpleCalculatorState createState() => _SimpleCalculatorState();
+}
+
+class _SimpleCalculatorState extends State<SimpleCalculator> {
+  String equation = "0";
+  String result = "0";
+  String expression = "";
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
+  void buttonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == "C") {
+        equation = "0";
+        result = "0";
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+      } else if (buttonText == "<-") {
+        equationFontSize = 48.0;
+        resultFontSize = 38.0;
+        equation = equation.substring(0, equation.length - 1);
+      } else if (buttonText == "=") {
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+        expression = equation;
+        expression = expression.replaceAll('×', '*');
+        expression = expression.replaceAll('÷', '/');
+
+        try {
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+          ContextModel cm = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+        } catch (e) {
+          result = "ERROR";
+        }
+      } else {
+        equationFontSize = 48.0;
+        resultFontSize = 38.0;
+        if (equation == "0") {
+          equation = buttonText;
+        } else {
+          equation = equation + buttonText;
+        }
+      }
+    });
+  }
+
+  Widget buildButton(
+      String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: FlatButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          side: BorderSide(
+            color: Colors.white,
+            width: 1,
+            style: BorderStyle.solid,
+          ),
+        ),
+        padding: EdgeInsets.all(16.0),
+        onPressed: () => buttonPressed(buttonText),
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "k",
+      home: Column(
+        children: <Widget>[
+          Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child:
+                  Text(equation, style: TextStyle(fontSize: equationFontSize))),
+          Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+              child: Text(result, style: TextStyle(fontSize: resultFontSize))),
+          Expanded(
+            child: Divider(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: Table(
+                  children: [
+                    TableRow(
+                      children: [
+                        buildButton("C", 1, Colors.redAccent),
+                        buildButton("<-", 1, Colors.redAccent),
+                        buildButton("÷", 1, Colors.blue),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton("7", 1, Colors.black),
+                        buildButton("8", 1, Colors.black),
+                        buildButton("9", 1, Colors.black),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton("4", 1, Colors.black),
+                        buildButton("5", 1, Colors.black),
+                        buildButton("6", 1, Colors.black),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton("1", 1, Colors.black),
+                        buildButton("2", 1, Colors.black),
+                        buildButton("3", 1, Colors.black),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton(".", 1, Colors.black),
+                        buildButton("0", 1, Colors.black),
+                        buildButton("%", 1, Colors.black),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.25,
+                child: Table(
+                  children: [
+                    TableRow(
+                      children: [
+                        buildButton("×", 1, Colors.blue), //Multiply
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton("-", 1, Colors.blue), //Minus
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton("+", 1, Colors.blue), //Add
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton("=", 2, Colors.blue), //Equal
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'calc.dart';
 import 'agecalc.dart';
 import 'bmicalc.dart';
 
@@ -8,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Kishan",
+      debugShowCheckedModeBanner: false,
+      title: "Pro Calculator",
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
@@ -23,81 +25,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int firstNum, secondNum;
-  String textToDisplay = "";
-  String result;
-  String operationToPerform;
-
-  void btnClicked(String btnText) {
-    if (btnText == "C") {
-      firstNum = 0;
-      secondNum = 0;
-      textToDisplay = "";
-      result = "";
-    } else if (btnText == "+" ||
-        btnText == "-" ||
-        btnText == "X" ||
-        btnText == "/") {
-      firstNum = int.parse(textToDisplay);
-      result = "";
-      operationToPerform = btnText;
-    } else if (btnText == "=") {
-      secondNum = int.parse(textToDisplay);
-      if (operationToPerform == "+") {
-        result = (firstNum + secondNum).toString();
-      }
-      if (operationToPerform == "-") {
-        result = (firstNum - secondNum).toString();
-      }
-      if (operationToPerform == "X") {
-        result = (firstNum * secondNum).toString();
-      }
-      if (operationToPerform == "/") {
-        result = (firstNum / secondNum).toString();
-      }
-    } else {
-      result = int.parse(textToDisplay + btnText).toString();
-    }
-
-    setState(() {
-      textToDisplay = result;
-    });
-  }
-
-  Widget cusutomButtonColor(String btnVal) {
-    return Expanded(
-        child: Container(
-      color: Colors.orange,
-      child: OutlineButton(
-        padding: EdgeInsets.all(25.0),
-        onPressed: () {
-          btnClicked(btnVal);
-        },
-        child: Text(
-          "$btnVal",
-          style: TextStyle(
-            fontSize: 25.0,
-          ),
-        ),
-      ),
-    ));
-  }
-
-  Widget cusutomButton(String btnVal) {
-    return Expanded(
-      child: OutlineButton(
-        padding: EdgeInsets.all(25.0),
-        onPressed: () {
-          btnClicked(btnVal);
-        },
-        child: Text(
-          "$btnVal",
-          style: TextStyle(fontSize: 25.0),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -121,14 +48,12 @@ class _HomePageState extends State<HomePage> {
                 title: Text('Age Calculator'),
                 onTap: () {
                   Navigator.of(context).pop();
-                  // Navigator.of(context).push(MaterialPageRoute(
-                  //     builder: (BuildContext context) => NewPage("Page two")));
                   Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
                 },
               ),
               ListTile(
                 leading: Icon(Icons.tag_faces),
-                title: Text('BMI Calculator'),
+                title: Text("BMI Calculator"),
                 onTap: () {
                   Navigator.of(context).pop();
                   Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
@@ -138,8 +63,8 @@ class _HomePageState extends State<HomePage> {
                 leading: Icon(Icons.format_line_spacing),
                 title: Text('Unit Converter'),
                 onTap: () {
-                  Navigator.of(context).pop();
-                  //////////////////////////////////////
+                  // Navigator.of(context).pop();
+                  // Navigator.push(context, MaterialPageRoute(builder: (context) => My2App()));
                 },
               ),
               ListTile(
@@ -153,57 +78,7 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
         ),
-        body: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.all(10.0),
-                  alignment: Alignment.bottomRight,
-                  child: Text(
-                    "$textToDisplay",
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  cusutomButton("9"),
-                  cusutomButton("8"),
-                  cusutomButton("7"),
-                  cusutomButtonColor("+"),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  cusutomButton("6"),
-                  cusutomButton("5"),
-                  cusutomButton("4"),
-                  cusutomButtonColor("-"),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  cusutomButton("3"),
-                  cusutomButton("2"),
-                  cusutomButton("1"),
-                  cusutomButtonColor("X"),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  cusutomButton("C"),
-                  cusutomButton("0"),
-                  cusutomButton("="),
-                  cusutomButtonColor("/"),
-                ],
-              ),
-            ],
-          ),
-        ));
+        body: Calculator(),
+      );
   }
 }
